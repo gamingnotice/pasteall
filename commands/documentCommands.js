d968fe66694ff316f0061500cb0b15a559946ef1
gamingnotice57 <aZWkFQYc@6!t)2>
2020-12-31 16:47:45 +0100

    ALL

---
+++ commands/documentCommands.js
@@ -0,0 +1,47 @@
+const {Command} = require("./commands");
+const fs = require("fs");
+const path = require("path");
+let documentStorage;
+
+const deleteDocumentCommand = new Command("deleteDocument", "Deletes a certain document", "deleteDocument <key>", async args => {
+    if (args.length === 1) {
+        const message = await documentStorage.delete(args[0]) ? "Successfully deleted document!" : "Document could not be deleted.";
+        console.log(message);
+        return true;
+    }
+    return false;
+});
+
+const readDocumentCommand = new Command("readDocument", "Reads a certain document", "readDocument <key>", async args => {
+    if (args.length === 1) {
+        const documentText = await documentStorage.load(args[0]);
+        if (documentText)
+            console.log(`Text of the document: ${documentText}.`);
+        else
+            console.log("Document could not be read.");
+        return true;
+    }
+    return false;
+});
+
+const createStaticDocumentCommand = new Command("createStaticDocument", "Creates a static document", "createStaticDocument <name> <contentFilePath>", async args => {
+    if (args.length === 2) {
+        const name = args[0];
+        try {
+            const text = fs.readFileSync(path.resolve(args[1]), {encoding: "utf-8"});
+
+            if (await documentStorage.save(name, "static", text, true))
+                console.log(`Successfully created static file '${name}'.`)
+        } catch (e) {
+            console.log("Error while reading the file!")
+        }
+        return true;
+    }
+    return false;
+});
+
+
+module.exports = storage => {
+    documentStorage = storage;
+    return [deleteDocumentCommand, readDocumentCommand, createStaticDocumentCommand];
+};
---
+++ commands/updateCommands.js
@@ -0,0 +1,32 @@
+const {Command} = require("./commands");
+let autoUpdater;
+
+const checkUpdateCommand = new Command("checkUpdate", "Checks for updates of the PasteServer", "checkUpdate [-dev]", async (args, provider) => {
+    const dev = args.length === 1 && args[0].toLowerCase() === "-dev";
+    if (args.length === 0 || dev) {
+        provider.enabled = false;
+        await autoUpdater.checkForUpdates(dev);
+        provider.enabled = true;
+        return true;
+    }
+    return false;
+});
+
+const installUpdateCommand = new Command("installUpdate", "Installs an update of the PasteServer, if available", "installUpdate [-dev]", async (args, provider) => {
+    const dev = args.length === 1 && args[0].toLowerCase() === "-dev";
+    if (args.length === 0 || dev) {
+        provider.enabled = false;
+        if (await autoUpdater.checkForUpdates(dev)) {
+            if (await autoUpdater.downloadUpdate(dev))
+                await autoUpdater.installUpdate();
+        }
+        provider.enabled = true;
+        return true;
+    }
+    return false;
+});
+
+module.exports = (updater) => {
+    autoUpdater = updater;
+    return [checkUpdateCommand, installUpdateCommand];
+};
